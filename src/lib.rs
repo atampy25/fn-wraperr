bda29ff249b905ce01e9d2b425f4bbcc8e6394e4
atampy25 <24306974+atampy25@users.noreply.github.com>
2023-10-29 12:53:27 +1000

    Use wrap_err instead of context

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
-//! This crate provides the [`context`](attr.context.html) macro for adding extra error
+//! This crate provides the [`wrap_err`](attr.wrap_err.html) macro for adding extra error
 //! information to a function.
 //!
-//! Works with [`anyhow`], [`failure`] and any other error type which
-//! provides a `context` method taking a string.
+//! Works with [`eyre`] and any other error type which
+//! provides a `wrap_err` method taking a string.
 //!
 //! ```
 //! # use std::fs::read_to_string;
 //! # use std::path::Path;
 //! # use std::io;
 //! #
-//! use fn_error_context::context;
+//! use fn_wrap_err::wrap_err;
 //!
-//! #[context("failed to parse config at `{}`", path.as_ref().display())]
+//! #[wrap_err("failed to parse config at `{}`", path.as_ref().display())]
 //! pub fn parse_config(path: impl AsRef<Path>) -> anyhow::Result<u32> {
 //!     let text = read_to_string(path.as_ref())?;
 //!     Ok(text.parse()?)
@@ -31,7 +31,6 @@
 //! [`anyhow`]: https://crates.io/crates/anyhow
 //! [`failure`]: https://crates.io/crates/failure
 
-#![doc(html_root_url = "https://docs.rs/fn-error-context/0.2.1")]
 #![deny(missing_docs)]
 
 extern crate proc_macro;
@@ -42,7 +41,7 @@ use quote::{quote, ToTokens};
 use syn::parse::{self, Parse, ParseStream};
 use syn::Token;
 
-/// Add context to errors from a function.
+/// Add wrap_err to errors from a function.
 ///
 /// The arguments to this macro are a format string with arguments using
 /// the standard `std::fmt` syntax. Arguments to the function can be used
@@ -55,7 +54,7 @@ use syn::Token;
 /// pub fn function() -> anyhow::Result<()> {
 ///     (|| -> anyhow::Result<()> {
 ///         function_body()
-///     })().map_err(|err| err.context("context"))
+///     })().map_err(|err| err.wrap_err("wrap_err"))
 /// }
 /// ```
 ///
@@ -63,15 +62,15 @@ use syn::Token;
 /// often be fixed by setting the `move` option of the attribute macro. For example:
 ///
 /// ```
-/// use fn_error_context::context;
+/// use fn_error_wrap_err::wrap_err;
 ///
-/// #[context(move, "context")]
+/// #[wrap_err(move, "wrap_err")]
 /// fn returns_reference(val: &mut u32) -> anyhow::Result<&mut u32> {
 ///     Ok(&mut *val)
 /// }
 /// ```
 #[proc_macro_attribute]
-pub fn context(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn wrap_err(args: TokenStream, input: TokenStream) -> TokenStream {
     let Args(move_token, format_args) = syn::parse_macro_input!(args);
     let mut input = syn::parse_macro_input!(input as syn::ItemFn);
 
@@ -90,7 +89,7 @@ pub fn context(args: TokenStream, input: TokenStream) -> TokenStream {
         let result = Ident::new("result", Span::mixed_site());
         quote! {
             let #result: #return_ty = async #move_token { #body }.await;
-            #result.map_err(|#err| #err.context(format!(#format_args)).into())
+            #result.map_err(|#err| #err.wrap_err(format!(#format_args)).into())
         }
     } else {
         let force_fn_once = Ident::new("force_fn_once", Span::mixed_site());
@@ -101,7 +100,7 @@ pub fn context(args: TokenStream, input: TokenStream) -> TokenStream {
             (#move_token || #return_ty {
                 ::core::mem::drop(#force_fn_once);
                 #body
-            })().map_err(|#err| #err.context(format!(#format_args)).into())
+            })().map_err(|#err| #err.wrap_err(format!(#format_args)).into())
         }
     };
     input.block.stmts = vec![syn::Stmt::Expr(syn::Expr::Verbatim(new_body), None)];
